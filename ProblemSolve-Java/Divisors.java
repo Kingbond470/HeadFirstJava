f364a9a7f3263ff6a51898b153907cd6a0586934
acuon <83219534+acuon@users.noreply.github.com>
2021-05-06 15:55:54 +0530

    Update Divisors.java

--- ProblemSolve-Java/Divisors.java
+++ ProblemSolve-Java/Divisors.java
@@ -1,3 +1,4 @@
+/*
 Masai Divisors
 
 Description
@@ -25,4 +26,23 @@ Sample Input 1
 Sample Output 1
 
 10
+*/
 
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        //System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        int i = 0;
+        for(i=l; i<=r; i++) {
+            if(i % k == 0) {
+                count = count + 1;
+            }
+        } 
+        System.out.println(count);       
+    }
+}
