05297508e6cd56de42436dfa1840a1fca809975e
acuon <83219534+acuon@users.noreply.github.com>
2021-05-07 16:33:29 +0530

    done

--- ProblemSolve-Java/RightAngleTriangle.java
+++ ProblemSolve-Java/RightAngleTriangle.java
@@ -1,3 +1,4 @@
+/*
 Right Angle Triangle
 
 Description
@@ -36,3 +37,25 @@ Sample Output 2
   *
 * *
 
+*/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        //System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<=n; j++) {
+                if(i+j==n || i+j>n) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
