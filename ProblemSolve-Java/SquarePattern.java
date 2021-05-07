eddf2d2db09ad4db97dbd324b41aa1dfda2480e9
acuon <83219534+acuon@users.noreply.github.com>
2021-05-07 16:13:26 +0530

    done

--- ProblemSolve-Java/SquarePattern.java
+++ ProblemSolve-Java/SquarePattern.java
@@ -1,3 +1,4 @@
+/*
 Square Pattern
 
 Description
@@ -37,3 +38,24 @@ Sample Output 2
 *   *
 * * * 
 
+*/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        //System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if(i==0 || j==0 || i==n-1 || j==n-1) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
