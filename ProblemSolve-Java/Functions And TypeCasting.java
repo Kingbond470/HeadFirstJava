e4cdb34db03ec7e48087e4d86772f80a0fae2ad7
acuon <83219534+acuon@users.noreply.github.com>
2021-05-13 19:04:12 +0530

    Done

--- ProblemSolve-Java/Functions And TypeCasting.java
+++ ProblemSolve-Java/Functions And TypeCasting.java
@@ -68,7 +68,6 @@ public class Main {
 
 //     Given a string remove the consonants from the string
 
-//Given a string remove the consonants from the string
 import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
@@ -87,7 +86,6 @@ public class Main {
 
 //     Given a number write a function to find if it is divisible by 7
 
-//Given a string remove the consonants from the string
 import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
@@ -108,20 +106,45 @@ public class Main {
 
 //     Given a word in upper case convert to lower case (Don't use inbuilt functions)
 
-//Given a word in upper case convert to lower case (Don't use inbuilt functions)
+
 import java.util.Scanner;
-public class Main {
-    public static void main(String args[]) {
+
+public class UpperToLower {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char s = sc.next().charAt(0);
-        l_U(s);    
+        String word = sc.nextLine();
+        System.out.println(toLowerCase(word));
     }
-    public static void l_U(char s) {
-        int n = (int)s-32;
-        char ch = (char)n;
-        System.out.println(ch);
+    public static String toLowerCase(String word) {
+        String output ="";
+        for(int i=0; i<word.length(); i++) {
+            output = output + toLower(word.charAt(i));
+        }
+        return output;
+    }
+    public static char toLower(char c) {
+        int a = (int)c;
+        char s1;
+        //65 to 90 -- ASCII values of A to Z
+        if(a>64 && a<91) {
+            a = (int)c + 32;
+            s1 = (char)a;
+        }
+        else {
+            s1 = c;
+        }
+        return s1;
     }
 }
+
+/*
+Input 
+RoHIT sHARmA
+
+Output
+rohit sharma
+*/
+
 
 
 //     Write a function to remove even numbers from the given array of numbers
@@ -146,10 +169,138 @@ public class Main {
 
 //     Given a string remove a given character from the string
 
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        char a = sc.next().charAt(0);
+        removeCharacter(word,a);
+        //System.out.println(word);
+        //System.out.println(a);
+    }
+    
+    public static void removeCharacter(String word, char a) {
+        String output = "";
+        for(int i=0; i<word.length(); i++) {
+            if(word.charAt(i) != a) {
+                output = output + word.charAt(i);
+            }
+        }
+        System.out.println(output);
+    }
+}
+/*
+Input
+GeeksforGeEks
+e
+
+Output
+GksforGEks
+*/
 
 //     Given a number write a function to find if it is divisible by 3
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    three(n);
+    
+    }
+    public static void three(int n) {
+        if(n%3==0) {
+            System.out.println("Divisible by 3");
+        }
+        else{
+            System.out.println("Not divisible by 3");
+        }
+    }
+}
 //     Use the function in the above statement to print the multiples of 3 till given limit
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    multipleOFthree(n);
+    
+    }
+    public static void multipleOFthree(int n) {
+        for(int i=1; i<=n; i++) {
+            if(i%3==0) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
+
+
+
 //     Given a single character in lower case convert to upper case (Don't use inbuilt functions)
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char s = sc.next().charAt(0);
+        l_U(s);    
+    }
+    public static void l_U(char s) {
+        int n = (int)s-32;
+        char ch = (char)n;
+        System.out.println(ch);
+    }
+}
+
 //     Write a function to check if a number is a prime
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    isPrime(n);
+    }
+    public static void isPrime(int n) {
+    if(n==0 || n==1) {
+        System.out.println(n+" is not a prime number");
+    }
+    else {
+        for(int i=2; i<n; i++) {
+            if(n%i == 0) {
+                System.out.println(n+" is not a prime number");
+                break;
+            }
+            else {
+                System.out.println(n+" is a prime number");
+                break;
+            }
+        }
+    }
+    }
+}
+
+
 //     Given a string remove the vowels from the string
 
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String word=sc.nextLine(); 
+    String output="";
+    int length=word.length();
+    for(int i=0; i<length; i++){
+        if(word.charAt(i)=='a' || word.charAt(i)=='e' || word.charAt(i)=='i' || word.charAt(i)=='o' || word.charAt(i)=='u' || word.charAt(i)=='A' || word.charAt(i)=='E' || word.charAt(i)=='I' || word.charAt(i)=='O' || word.charAt(i)=='U'){
+            continue;
+        }
+        else {
+            output=output+word.charAt(i);
+        }
+    }System.out.print(output); 
+    }
+}
