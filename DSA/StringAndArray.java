69ac3eb7c96995ff6f770310bb7b2ac7eeb1077b
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-02 10:38:18 +0530

    Array and String Problem
    Online Judge

---
+++ DSA/StringAndArray.java
@@ -0,0 +1,155 @@
+Problem -1
+Longest Repeated Odd
+
+Description
+
+You are given an array A of N integers. Your task is to find the maximum number of times an odd number is continuously repeated in the array.
+
+Input
+
+Input Format
+
+First line contains N which is the number of element present in the array.
+
+Second line contains N integers which are the values of array.
+
+Constraints
+
+N<100
+
+Output
+
+Output Format
+
+Output one integer which the maximum number of times an odd number is repeated in array.
+
+Sample Input 1
+
+12
+1 1 1 1 2 2 2 2 2 1 1 1
+
+Sample Output 1
+
+4
+
+Hint
+
+Sample 1 Explanation
+
+1 is repeated 4 times from index 0 to index 3 => 4 times
+
+2 is repeated 5 times from index 4 to index 8 => 5 times
+
+1 is repeated 3 times from index 9 to index 11 => 3 times
+
+So, the output is 4 since 1 is odd.
+
+  
+Problem -2
+  Masai Palindromic Substring
+
+Description
+
+You are provided a string S. Write a program that returns length of the longest palindromic substring of that string.
+
+Input
+
+Input Format
+
+You are provided with a string
+
+Constraints
+
+Length of string < 100
+
+Output
+
+Output Format
+
+Output one number which is length of the longest substring which is a palindrome
+
+Sample Input 1
+
+thisracecarisgood
+
+Sample Output 1
+
+7
+
+Hint
+
+Sample 1 Explanation
+
+
+Problem -3
+  Day of the Week
+
+Description
+
+Given the current day, and a number N, find what day will it be after N days.
+
+Note:Current day will be from the set ->{"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"}
+
+Input
+
+The first line of the input contains the name of the current day.
+
+The second line of the input contains N.
+
+Constraints
+
+1 <= N <= 1000
+
+Output
+
+Print the name of the day, which will occur after N days.
+
+Sample Input 1
+
+Wednesday
+8
+
+Sample Output 1
+
+Thursday
+
+Hint
+
+In the sample test case, the current day is Wednesday. After N = 8 days, it will be Thursday.
+
+  
+problem -4
+ Rotate the array - easy version
+
+Description
+
+You are given an array of n elements and an integer k, you need to rotate the array by k units towards right direction ( => ).
+
+Input
+
+1<=T<=10
+
+1<=N<=1000, 0<=K<=1000
+
+1 <= Ai <= 1000000
+
+Output
+
+out put N elements, elements of the array , rotated by K units
+
+Sample Input 1
+
+3
+3 1
+1 2 3
+2 2
+1 2
+2 3
+1 2
+
+Sample Output 1
+
+3 1 2
+1 2
+2 1
+
