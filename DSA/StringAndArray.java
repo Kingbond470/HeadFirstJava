ad7919ddbb55bb1a68eea93dc93db935828be4e7
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-02 13:04:32 +0530

    Update StringAndArray.java

--- DSA/StringAndArray.java
+++ DSA/StringAndArray.java
@@ -1,3 +1,4 @@
+/*
 Problem -1
 Longest Repeated Odd
 
@@ -43,8 +44,47 @@ Sample 1 Explanation
 1 is repeated 3 times from index 9 to index 11 => 3 times
 
 So, the output is 4 since 1 is odd.
+*/
 
-  
+import java.util.Scanner;
+public class LongestContinuousRepeatedOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+        for(int i=0; i<n; i++) {
+            ar[i] = sc.nextInt();
+        }
+        int count=0;
+        int max=0;
+
+        for(int i=0; i<ar.length; i++) {
+            if(ar[i]%2==1) {
+                if(i==0) {
+                    count=1;
+                }
+                else if(ar[i]==ar[i-1]) {
+                    if(count==0) {
+                        count = 1;
+                    }
+                    count++;
+                }
+            }
+            else{
+                count=0;
+                continue;
+            }
+            if (count > max) {
+                max = count;
+            }
+        }
+        System.out.println(max);
+    }
+}
+
+
+
+/*
 Problem -2
   Masai Palindromic Substring
 
@@ -79,8 +119,12 @@ Sample Output 1
 Hint
 
 Sample 1 Explanation
+*/
 
 
+
+
+/*
 Problem -3
   Day of the Week
 
@@ -116,8 +160,42 @@ Thursday
 Hint
 
 In the sample test case, the current day is Wednesday. After N = 8 days, it will be Thursday.
+*/
 
-  
+import java.util.Scanner;
+
+public class DayOftheWeek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String given_day = sc.next();
+        int given_num = sc.nextInt();
+        int toFind = given_num%7;
+        int n=0;
+        switch(given_day) {
+            case("Monday"):n=0;break;
+            case("Tuesday"):n=1;break;
+            case("Wednesday"):n=2;break;
+            case("Thursday"):n=3;break;
+            case("Friday"):n=4;break;
+            case("Saturday"):n=5;break;
+            case("Sunday"):n=6;break;
+        }
+        toFind = toFind + n;
+        toFind = toFind%7;
+        switch(toFind) {
+            case 0:System.out.println("Monday");break;
+            case 1:System.out.println("Tuesday");break;
+            case 2:System.out.println("Wednesday");break;
+            case 3:System.out.println("Thursday");break;
+            case 4:System.out.println("Friday");break;
+            case 5:System.out.println("Saturday");break;
+            case 6:System.out.println("Sunday");break;
+        }
+    }
+}
+
+
+/*
 problem -4
  Rotate the array - easy version
 
@@ -152,4 +230,36 @@ Sample Output 1
 3 1 2
 1 2
 2 1
+*/
+
+
+import java.util.Scanner;
+
+public class RotatetheArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCases = sc.nextInt();
+        for(int i=0; i<testCases; i++) {
+            int len = sc.nextInt();
+            int toRotate = sc.nextInt();
+            int[] ar = new int[len];
+            for(int j=0; j<ar.length; j++) {
+                ar[j] = sc.nextInt();
+            }
+            for(int j=0; j<toRotate; j++) {
+                int last = ar[ar.length-1];
+                for(int k=ar.length-1; k>0; k--) {
+                    //int temp = ar[k];
+                    ar[k] = ar[k-1];
+                    //ar[k-1] = temp;
+                }
+                ar[0] = last;
+            }
+            for(int j=0; j<ar.length; j++) {
+                System.out.print(ar[j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
 
