7cb02d40cc522984daef8e1c898ac7540d525c07
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-05 20:33:25 +0530

    done <-arraySort, substring, duplicate(HashSet)->

--- ProblemSolve-Java/Collections.java
+++ ProblemSolve-Java/Collections.java
@@ -184,6 +184,23 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
+
+//  ------Using only HashSet----------------
+// public class removeDuplicates {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int i=0; i<n; i++) {
+//             set.add(sc.nextInt());
+//         }
+//         System.out.println(set);
+//     }
+// }
+//  ---------------------------------------
+
+
+// i had to take the inputs in array than remove the duplicates
 public class removeDuplicates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
