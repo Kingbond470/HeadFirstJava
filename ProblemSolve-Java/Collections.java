0177d949e6be09a4bb1b6020af72e2530a23e3b7
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-05 16:55:37 +0530

    Collection Problem

---
+++ ProblemSolve-Java/Collections.java
@@ -0,0 +1,80 @@
+ Problem -1
+
+    A string s is nice if, for every letter of the alphabet that s contains, it appears both in uppercase and lowercase. For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not because 'b' appears, but 'B' does not.
+
+Given a string s, return the longest substring of s that is nice. If there are multiple, return the substring of the earliest occurrence. If there are none, return an empty string.
+
+Example 1:
+
+Input: s = "YazaAay"
+Output: "aAa"
+Explanation: "aAa" is a nice string because 'A/a' is the only letter of the alphabet in s, and both 'A' and 'a' appear.
+"aAa" is the longest nice substring.
+
+Example 2:
+
+Input: s = "Bb"
+Output: "Bb"
+Explanation: "Bb" is a nice string because both 'B' and 'b' appear. The whole string is a substring.
+
+Example 3:
+
+Input: s = "c"
+Output: ""
+Explanation: There are no nice substrings.
+
+Example 4:
+
+Input: s = "dDzeE"
+Output: "dD"
+Explanation: Both "dD" and "eE" are the longest nice substrings.
+As there are multiple longest nice substrings, return "dD" since it occurs earlier.
+
+  Problem -2
+  
+    A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3". Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+Example 1:
+
+Input: s = "is2 sentence4 This1 a3"
+Output: "This is a sentence"
+Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+
+Example 2:
+
+Input: s = "Myself2 Me1 I4 and3"
+Output: "Me Myself and I"
+Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
+
+ Problem -3
+  Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+Problem -4   
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new array.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:  nums = [1,2]
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: nums = [0,1,2,3,4]
+
