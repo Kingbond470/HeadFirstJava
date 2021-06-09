c9fe96273fa21471d8b0163c9d6d1be24fe4eb06
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-09 12:14:50 +0530

    Two D Array |  09 June 2021 Problem
    Using 2D array

---
+++ DSA/2DArray.java
@@ -0,0 +1,121 @@
+Problem -1
+Result or Tie - Tic Tac Toe
+
+Description
+
+You are given a 3*3 matrix (2-dimensional array) that represents the final situation of a Tic Tac Toe.
+
+Your task is to write a program that finds out the winner in case the match ended in a win. If the winner is 'x', print 'x'(without quotes).
+
+If the winner is 'o', print 'o' (without quotes)
+
+In case the match ended in a tie, print "Tie" (without quotes)
+
+Input
+
+InputFormat
+
+The input contains 3 lines where each line has 3 space separated characters which represent the final status of tic tac toe
+
+Constraints
+
+The input contains either of the 2 alphabets: 'x' and 'o'
+
+Output
+
+Print Tie/x/o depending on the end result of the game.
+
+Sample Input 1
+
+x o x
+o x x
+o o o
+
+Sample Output 1
+
+o`
+
+Problem -2
+Transpose the Matrix
+
+Description
+
+The transpose of a matrix is an operator which flips a matrix over its diagonal, that is it switches the row and column of the matrix by producing another matrix.
+
+See sample test case for better understanding
+
+So, given an array of n rows and m columns, generate the transpose of the matrix.
+
+Input
+
+Input Format:
+
+First line contains two integers n and m
+
+Next n lines contain m integers which represents each row of 2d array
+
+Constraints:
+
+n,m <= 500
+
+Output
+
+Print the transposed array
+
+Sample Input 1
+
+5 4
+0 0 0 0
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+
+Sample Output 1
+
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+
+Problem -3
+Rotate By 90
+
+Description
+
+Given a square matrix, turn it by 90 degrees in an anti-clockwise direction
+
+Input
+
+Input Format
+
+First line will contain the number of rows n (same as the number of columns)
+
+After that next n lines will n rows of the matrix.
+
+Constraints
+
+n<=1000
+
+Elements Of the Matrix <= 10000
+
+
+Output
+
+You have to display the rotated matrix.
+
+Sample Input 1
+
+4
+1 2 3 4
+5 6 7 8
+1 2 3 4
+5 6 7 8
+
+Sample Output 1
+
+4 8 4 8
+3 7 3 7
+2 6 2 6
+1 5 1 5
+
