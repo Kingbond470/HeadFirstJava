d8c5d0fd14b33231fc31894a2b87f6e2782c903f
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-15 21:22:57 +0530

    min steps to make all array element zero

--- DSA/2D_Array/ArrayOfZero.java
+++ DSA/2D_Array/ArrayOfZero.java
@@ -3,7 +3,8 @@ Array of Zeroes
 
 Description
 
-You have an array of non-negative integers. You can perform one operation on it in which you can choose i-th index with 0 <= i < n and subtract 1 from both i-th and (i+1) th index of array.
+You have an array of non-negative integers. You can perform one operation on it in which you can
+choose i-th index with 0 <= i < n and subtract 1 from both i-th and (i+1) th index of array.
 
 Your task is to write a program that can comment if it is possible to get an array of all zeros after several operations.
 
@@ -45,50 +46,27 @@ YES
 import java.util.*;
 
 class Main {
-
   public static void main(String[] args) {
-
     Scanner sc = new Scanner(System.in);
-
     int n = sc.nextInt();
-
     int[] ar = new int[n];
-
     for(int i=0; i<n; i++) {
-
       ar[i] = sc.nextInt();
-
     }
-
     for(int i=0; i<n-1; i++) {
-
       int temp = ar[i];
-
       ar[i+1] = ar[i+1] - temp;
-
       ar[i] = ar[i] - temp;
-
       //System.out.println(ar[i-1]+" "+ar[i]);
-
     }
-
     boolean b = true;
-
     for(int i=0; i<n; i++) {
-
       if(ar[i]!=0) {
-
         b = false;
-
         break;
-
       }
-
     }
-
     System.out.println(b?"YES":"NO");
-
   }
-
 }
 
