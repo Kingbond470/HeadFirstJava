199a58445eb3c791da8fd56f80d0c564d6c72006
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-15 22:02:41 +0530

    problem 3 | Beach Fun

--- DSA/2D_Array/DSA_1/03_BeachFun.java
+++ DSA/2D_Array/DSA_1/03_BeachFun.java
@@ -1,6 +1,7 @@
 /*
 Problem-3
 Beach Fun
+
 Description
 You are on a vacation in Goa, and you want to visit a beach. Now you have two options, you can either go to "RK Beach", or "RU Beach".
 You are short on time, so you can visit only one beach. You are given the position of the two beaches, RK Beach is located at coordinate 1,
@@ -22,10 +23,12 @@ Sample Input 1
 23
 Sample Output 1
 RK Beach
+
 Sample Input 2
 29
 Sample Output 2
 RU Beach
+
 Hint
 In the first sample test case, the position X is 23, therefore the distance from RK Beach is 23 - 1 = 22, and the distance from RU Beach is 49 - 23 = 26. Therefore, the beach closer to you is RK Beach.
 In the second sample test case, the position X is 29, therefore the distance from RK Beach is 29 - 1 = 28, and the distance from RU Beach is 49 - 29 = 20. Therefore, the beach closer to you is RU Beach.
