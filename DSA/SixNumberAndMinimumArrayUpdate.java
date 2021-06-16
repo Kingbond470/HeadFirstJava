bce1692c1cc22f6b99def4c117c0e75a89d8be7b
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-16 10:55:26 +0530

    New Program | Six Number & Minimum Array Update

---
+++ DSA/SixNumberAndMinimumArrayUpdate.java
@@ -0,0 +1,74 @@
+Such Six Numbers
+
+Description
+
+You are given   N  integers, your task is to count group of six such numbers among those   N  integers which follow this equation:
+
+
+Image
+
+d ≠ 0
+
+Input
+
+Input Format :
+
+First line contains integer N
+
+Next N line contains N distinct integers (one integer per line)
+
+Constraints :
+
+1 ≤ N ≤ 100
+
+Numbers present in array range between -30000 and 30000
+
+Output
+
+Output the total number of plausible group of six numbers.
+
+Sample Input 1
+
+3
+5
+7
+10
+
+Sample Output 1
+
+10
+
+Problem -2
+Minimum Array Update Required
+
+Description
+
+You are given an array having N integer values. You are also provided an integer K. You can update every element of the array by incrementing it by 1 in 1 second. Your task is to write a program that calculates the minimum time required (in seconds) to make every element of the array greater than or equal to K.
+
+Input
+
+Input Format:
+
+First line of the input contains two integer N and K
+
+Second line contains N elements separated by space.
+
+Constraints:
+
+N < 100000
+
+K < 100000
+
+Output
+
+Output one number, which is the minimum number of steps required to make every element of the array greater than or equal to K.
+
+Sample Input 1
+
+3 4
+1 2 5
+
+Sample Output 1
+
+3
+
